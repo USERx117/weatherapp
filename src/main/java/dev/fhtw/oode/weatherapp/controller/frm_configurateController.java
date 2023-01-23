9567eb1e389c5cd2c3ba1e586ed60223cd7ffb90
Florian <fgotschim@gmail.com>
2023-01-23 19:05:41 +0100

    everything

---
+++ src/main/java/dev/fhtw/oode/weatherapp/controller/frm_configurateController.java
@@ -0,0 +1,95 @@
+package dev.fhtw.oode.weatherapp.configurate;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import dev.fhtw.oode.weatherapp.client.OpenWeatherClient;
+import dev.fhtw.oode.weatherapp.client.OpenWeatherConfigReader;
+import dev.fhtw.oode.weatherapp.service.ConfigUpdater;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class frm_configurateController {
+
+    @FXML
+    private Button btn_save;
+    @FXML
+    private Button btn_close;
+    @FXML
+    private Label lbl_location;
+    @FXML
+    private Label lbl_apiKey;
+    @FXML
+    private Label lbl_configTitle;
+    @FXML
+    private RadioButton rbtn_metric;
+    @FXML
+    private RadioButton rbtn_imperial;
+    @FXML
+    private TextField tf_longitude;
+    private String longitude;
+    @FXML
+    private TextField tf_latitude;
+    private String latitude;
+    @FXML
+    private TextField tf_apiKey;
+    private String apiKey;
+    @FXML
+    private TextField tf_interval;
+    private String interval;
+    @FXML
+    private Label lbl_perMin;
+    @FXML
+    private Label lbl_updateInterval;
+
+    @FXML
+    void btn_saveButtonClicked(ActionEvent event) {
+        String unit = rbtn_metric.isSelected() ? "metric" : "imperial";
+        longitude = tf_longitude.getText();
+        latitude = tf_latitude.getText();
+        apiKey = tf_apiKey.getText();
+        interval = tf_interval.getText();
+
+        try {
+            FileWriter writer = new FileWriter("src/app.config");
+            OpenWeatherConfigReader owcr = new OpenWeatherConfigReader("src/app.config");
+            Properties prop = owcr.getProperties();
+            prop.setProperty("openweather_api_key", apiKey);
+            prop.setProperty("openweather_longitude", longitude);
+            prop.setProperty("openweather_latitude", latitude);
+            prop.setProperty("openweather_updateInterval", interval);
+            prop.setProperty("openweather_api_hostname", "api.openweathermap.org");
+            prop.setProperty("openweather_units", unit);
+
+            prop.store(writer, "Properties updated!");
+            btn_closeButtonClicked(event);
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @FXML
+    void rbtn_metric_Action(ActionEvent event) {
+
+    }
+    @FXML
+    void rbtn_imperial_Action(ActionEvent event) {
+
+    }
+
+    public void btn_closeButtonClicked(ActionEvent actionEvent) {
+        Stage stage = (Stage) btn_close.getScene().getWindow();
+        stage.close();
+    }
+}
---
+++ src/main/java/dev/fhtw/oode/weatherapp/controller/frm_weatherappmainController.java
@@ -0,0 +1,157 @@
+package dev.fhtw.oode.weatherapp;
+
+import dev.fhtw.oode.weatherapp.client.OpenWeatherClient;
+import dev.fhtw.oode.weatherapp.client.OpenWeatherConfigReader;
+import dev.fhtw.oode.weatherapp.model.WeatherEntity;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class frm_weatherappmainController implements Initializable{
+
+    private WeatherEntity weatherData;
+
+    @FXML
+    private Label lbl_currTemp;
+    @FXML
+    private Label lbl_currLocation;
+    @FXML
+    private Label lbl_currDate;
+    @FXML
+    private Label lbl_currPressure;
+    @FXML
+    private Label lbl_currHigh;
+    @FXML
+    private Label lbl_currLow;
+    @FXML
+    private Label lbl_foreDateDayName1;
+    @FXML
+    private Label lbl_foreDateDayName2;
+    @FXML
+    private Label lbl_foreDateDayName3;
+    @FXML
+    private Label lbl_foreDateDayName4;
+    @FXML
+    private Label lbl_foreDateDayName5;
+    @FXML
+    private Label lbl_foreD1High;
+    @FXML
+    private Label lbl_foreD1Low;
+    @FXML
+    private Label lbl_foreD2High;
+    @FXML
+    private Label lbl_foreD2Low;
+    @FXML
+    private Label lbl_foreD3High;
+    @FXML
+    private Label lbl_foreD3Low;
+    @FXML
+    private Label lbl_foreD4High;
+    @FXML
+    private Label lbl_foreD4Low;
+    @FXML
+    private Label lbl_foreD5High;
+    @FXML
+    private Label lbl_foreD5Low;
+    @FXML
+    private Label lbl_title;
+    @FXML
+    private Button bt_Configure;
+    @FXML
+    private Button bt_Update;
+
+
+
+    @FXML
+    void bt_ConfigureButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bt_Configure_Clicked(MouseEvent event) throws IOException {
+
+        try {
+            Stage stage_location = new Stage();
+            FXMLLoader locationscene_fxmlLoader = new FXMLLoader(WeatherApplication.class.getResource("frm_configurate.fxml"));
+            Scene scene_location = new Scene(locationscene_fxmlLoader.load(), 800,400);
+            stage_location.setTitle("Location Finder");
+            stage_location.setScene(scene_location);
+            stage_location.show();
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    @FXML
+    void bt_UpdateButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bt_Update_Clicked(MouseEvent event) {
+        initialize(null, null);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fetchWeatherData();
+        this.lbl_currTemp.setText(bigDecimal(this.weatherData.getTemperature())+"°C");
+        this.lbl_currDate.setText(dateConvert(this.weatherData.getDate()));
+        this.lbl_currLocation.setText(this.weatherData.getLocation());
+        this.lbl_currPressure.setText(this.weatherData.getPressure()+"mbar");
+        this.lbl_currHigh.setText(bigDecimal(this.weatherData.getTemp_max())+"°C");
+        this.lbl_currLow.setText(bigDecimal(this.weatherData.getTemp_min())+"°C");
+    }
+
+    private void fetchWeatherData() {
+        try {
+            OpenWeatherConfigReader owcr = new OpenWeatherConfigReader("src/app.config");
+            Properties prop = owcr.getProperties();
+            OpenWeatherClient owc = new OpenWeatherClient(
+                    prop.getProperty("openweather_api_key"),
+                    prop.getProperty("openweather_api_hostname"),
+                    prop.getProperty("openweather_units"),
+                    prop.getProperty("openweather_longitude"),
+                    prop.getProperty("openweather_latitude"));
+            this.weatherData = owc.connect().getCurrent();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String bigDecimal(String decimal){
+        BigDecimal d = new BigDecimal(decimal).setScale(0, RoundingMode.DOWN);
+        return String.valueOf(d);
+    }
+
+    private String dateConvert(String currentDate) {
+        long converted = Long.parseLong(currentDate+"000");
+        DateFormat obj = new SimpleDateFormat("E dd.MM.yyyy");
+        Date res = new Date(converted);
+        return obj.format(res);
+    }
+}
